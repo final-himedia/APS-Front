502dbd446ea03d04ed83bba2b86a21d7323e5414
kms <komi254@naver.com>
2025-06-11 17:29:15 +0900

    feat:프로트 생산라우팅,작업도구마스터,판매오더 조회 기능 완료

--- src/app/scenario/resource/tool-master/page.js
+++ src/app/scenario/resource/tool-master/page.js
@@ -1,10 +1,62 @@
 "use client";
 
-export default function BopPage() {
+import { Box, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+
+const columns = [
+  { field: "id", headerName: "순번" },
+  { field: "siteId", headerName: "플랜트" },
+  { field: "toolId", headerName: "작업장 코드" },
+  { field: "toolState", headerName: "호기명" },
+  { field: "toolCavity", headerName: "호기그룹" },
+  { field: "scenarioId", headerName: "호기유형" },
+  { field: "toolName", headerName: "우선순위 그룹" },
+];
+
+export default function ToolMaster() {
+  const [rows, setRows] = useState([]);
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 10,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/scenarios/resource/tool-master")
+      .then((res) => res.json())
+      .then((data) => {
+        const list = data.toolMasters || [];
+
+        const formatted = list.map((item, index) => ({
+          id: index + 1,
+          siteId: item.siteId,
+          toolId: item.toolId,
+          toolState: item.toolState,
+          toolCavity: item.toolCavity,
+          scenarioId: item.scenarioId,
+          toolName: item.toolName,
+        }));
+
+        setRows(formatted);
+      });
+  }, []);
+
   return (
-    <div style={{ padding: 16 }}>
-      <h2>작업도구 마스터</h2>
-      <p>여기에 작업도구 마스터 관련 테이블 넣기</p>
-    </div>
+    <Box p={2}>
+      <Typography variant="h6" gutterBottom>
+        작업도구 마스터
+      </Typography>
+
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        onPaginationModelChange={(model) => setPaginationModel(model)}
+        pageSizeOptions={[5, 10, 25, 50, 100]}
+        checkboxSelection
+        sx={{ border: 0 }}
+        rowHeight={38}
+      />
+    </Box>
   );
 }
--- src/app/scenario/target/demand/page.js
+++ src/app/scenario/target/demand/page.js
@@ -1,10 +1,79 @@
 "use client";
 
-export default function BopPage() {
+import { Box, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+
+const columns = [
+  { field: "id", headerName: "순번" },
+  { field: "demandId", headerName: "판매오더번호" },
+  { field: "siteId", headerName: "플랜트" },
+  { field: "partId", headerName: "품목코드" },
+  { field: "partName", headerName: "품목명" },
+  { field: "customerId", headerName: "고객사" },
+  { field: "dueDate", headerName: "납기일" },
+  { field: "demandQty", headerName: "주문수량" },
+  { field: "priority", headerName: "우선순위" },
+  { field: "uom", headerName: "단위" },
+  { field: "orderType", headerName: "주문유형" },
+  { field: "orderTypeName", headerName: "주문유형내역" },
+  { field: "exceptYn", headerName: "제외주문" },
+  { field: "headerCreationDate", headerName: "오더생성일" },
+  { field: "hasOverActQty", headerName: "초과실적보유주문FLAG" },
+  { field: "scenarioId", headerName: "시나리오" },
+];
+
+export default function demand() {
+  const [rows, setRows] = useState([]);
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 10,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/scenarios/target/demand")
+      .then((res) => res.json())
+      .then((data) => {
+        const list = data.demands || [];
+
+        const formatted = list.map((item, index) => ({
+          id: index + 1,
+          demandId: item.demandId?.demandId,
+          siteId: item.demandId?.siteId || "",
+          partId: item.demandId?.partId || "",
+          partName: item.partName,
+          customerId: item.customerId,
+          dueDate: item.dueDate,
+          demandQty: item.demandQty,
+          priority: item.priority,
+          uom: item.uom,
+          orderType: item.orderType,
+          orderTypeName: item.orderTypeName,
+          exceptYn: item.exceptYn,
+          headerCreationDate: item.headerCreationDate,
+          hasOverActQty: item.hasOverActQty,
+          scenarioId: item.scenarioId,
+        }));
+
+        setRows(formatted);
+      });
+  }, []);
+
   return (
-    <div style={{ padding: 16 }}>
-      <h2>판매오더</h2>
-      <p>여기에 판매오더 관련 테이블 넣기</p>
-    </div>
+    <Box p={2}>
+      <Typography variant="h6" gutterBottom>
+        판매 오더
+      </Typography>
+
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+        rowHeight={38}
+      />
+    </Box>
   );
 }
