513648c6e1cc3a3a10a892be881a82cf5f53e394
dap <ozalice011202@gmail.com>
2025-06-12 10:00:39 +0900

    fetch : 툴바 layout에 적용

--- src/app/scenario/page.js
+++ src/app/scenario/page.js
@@ -7,10 +7,6 @@ import Toolbar from "../standard/Toolbar";
 export default function ScenarioPage() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
-      {/* 툴바 */}
-      <Box sx={{ mt: 2 }}>
-        <Toolbar />
-      </Box>
 
       {/* 가운데 콘텐츠 (스크롤 영역 포함) */}
       <Box sx={{ flex: 1, overflow: "auto", p: 2 }}>
