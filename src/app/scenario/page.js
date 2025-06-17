710b07d474d4d8390512e8ae59aa7a8516875893
dap <ozalice011202@gmail.com>
2025-06-17 17:51:06 +0900

    fix : 게시판, 시나리오 수정

--- src/app/scenario/page.js
+++ src/app/scenario/page.js
@@ -1,13 +1,5 @@
-"use client";
-
-import { Box } from "@mui/material";
-import DataGridSection from "../standard/DataGridSection"; // ✅ 핵심 콘텐츠만 불러오기
+import { redirect } from "next/navigation";
 
 export default function ScenarioPage() {
-  return (
-    <Box sx={{ display: "flex", height: "100%" }}>
-      {/* 시나리오 목록은 layout에서 관리, 여긴 콘텐츠만 */}
-      <DataGridSection />
-    </Box>
-  );
+  redirect("/scenario/target/demand");
 }
